0a4d738cebf1efff9297aeca7d4752bb1734ff8f
gonzalosinnott <gonzalo.sinnott@gmail.com>
2021-08-27 10:16:15 -0300

    Creacion Categoria

---
+++ Agenda/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda.Models
+{
+    public class Category
+    {
+        [Key]
+        
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obligatory Field")]
+        [StringLength(15, ErrorMessage ="{0} The field must have between {2} and {1}", MinimumLength = 4)]
+        public string Name { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Creation Date")]
+        public DateTime? CreationDate { get; set; }
+    }
+}
